a019b1d146b520247cd203d057f0c2d1b3d00e7d
Adzzio <105452171+Adzzio@users.noreply.github.com>
2022-11-18 19:45:03 +0100

    Delete obiektowo zad 5 directory

--- obiektowo zad 5/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once dirname(__FILE__).'/config.php';
-
-//przekazanie żądania do następnego dokumentu ("forward")
-require_once _ROOT_PATH.'/app/lib/smarty/Smarty.class.php';
-include _ROOT_PATH."/app/calc.php";
